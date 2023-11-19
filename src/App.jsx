d54c75fe90cde2d8e18d9935a157ac0fd3750fcb
TheOneLegacy <legacyisbaddie@gmail.com>
2023-11-19 13:38:07 +0100

    final deploy?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/shared/Layout";
 import "./App.css";
