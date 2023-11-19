f672077f0a1990cade26d2c39ab714853a01edc8
TheOneLegacy <legacyisbaddie@gmail.com>
2023-11-19 13:41:46 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // Use a relative path or your domain if deploying to a subpath
+  base: "/neotr-portfolio/",
   plugins: [react()],
 });
