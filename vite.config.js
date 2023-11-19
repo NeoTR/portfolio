15c7fb446bc6aa5d2223b5829d0ccc54606f1df9
TheOneLegacy <legacyisbaddie@gmail.com>
2023-11-19 13:46:00 +0100

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/neotr-portfolio/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/components": "/src/components/", // Adjust the path accordingly
+    },
+  },
 });
