7197cbb14959dad22f9404d4686b482241fd2571
TheOneLegacy <legacyisbaddie@gmail.com>
2023-11-25 16:16:54 +0100

    i think this will work

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../NavBar";
+import Nav from "../Nav";
 import Landing from "../Landing";
 import AboutMe from "../AboutMe";
 import Skills from "../Skills";
@@ -10,7 +10,7 @@ import Footer from "../Footer";
 export default function Layout() {
   return (
     <div className="bg-neutral-100 h-screen w-screen flex ">
-      <NavBar />
+      <Nav />
       <div className="bg-neutral-100 w-screen h-screen mt-[80px]">
         <Landing />
         <AboutMe />
